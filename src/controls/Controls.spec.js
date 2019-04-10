3d7868b1d74a4aac5c12f6ef32ddc2733429d0a1
JaySebagh <jaysebagh@gmail.com>
2019-04-10 15:49:04 -0700

    started some testing

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,16 @@
 // Test away!
+
+import React from 'react';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+import renderer from 'react-test-renderer';
+import Controls from './Controls.js';
+import 'jest-dom/extend-expect';
+
+afterEach(cleanup)
+
+it("Open Gate fires callback on click", () => {
+    const closedFunction = jest.fn();
+    const { getByTestId } = render(<Controls toggleClosed={closedFunction}/>);
+    fireEvent.click(getByTestId('openButton'))
+    expect(closedFunction).toHaveBeenCalled();
+})
